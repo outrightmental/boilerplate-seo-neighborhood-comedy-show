484a847b0fba687ba48e9a028dc4b47ed723ea6e
Nick Kaye <nick.c.kaye@gmail.com>
2013-08-22 14:49:44 -0400

    The simplest possible PHP page.

---
+++ index.php
@@ -0,0 +1,187 @@
+<?php
+
+date_default_timezone_set('America/New_York');
+
+$parent_name = "Dean Street Brooklyn";
+$name = "Dean Street Comedy";
+$title = "Comedy Night at Dean Street, Brooklyn, NY";
+$description = "Top NYC comedy talent at Brooklyn prices! A couple can enjoy a comedy show for 2 hours, drink and eat well for $50. After party at the bar!";
+$url = "http://www.deanstreetcomedy.com/";
+$dateslug = '20130814';
+$postercopy = "Dean Street Comedy. Wednesday, August 14.  9:00PM (Doors 8:45).  Your Host: Alissa Bissonnette.  Featuring Jermaine Fowler and Ben Kissel.  Make 'em laugh!  www.DeanStreetComedy.com. 755 Dean Street, Brooklyn, NY.";
+$ga_account = "UA-43069052-1";
+$twitter = "deanstreetbk";
+$gplus_publisher = "https://plus.google.com/113786691084626528718/posts";
+$url_facebook = "https://www.facebook.com/163065380378627";
+$url_youtube = "http://www.youtube.com/deanstreetcomedy";
+$url_twitter = "http://www.twitter.com/$twitter/";
+$url_foursquare = "https://foursquare.com/v/dean-street/4d3933e376d1a35dfd3d058e";
+
+?><!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title><?php echo $title; ?></title>
+    <meta name="description" content="<?php echo $description; ?>"/>
+    <link rel="canonical" href="<?php echo $url; ?>"/>
+    <link rel="publisher" href="<?php echo $gplus_publisher; ?>"/>
+    <meta property='og:locale' content='en_US'/>
+    <meta property='og:type' content='article'/>
+    <meta property='og:title' content="<?php echo $title; ?>"/>
+    <meta property='og:description' content='<?php echo $description; ?>'/>
+    <meta property='og:url' content='<?php echo $url; ?>'/>
+    <meta property='og:site_name' content='<?php echo $name; ?>'/>
+    <meta property='article:author' content='<?php echo $url_facebook; ?>'/>
+    <meta property='article:publisher' content='<?php echo $url_facebook; ?>'/>
+    <meta property='fb:admins' content='518634848'/>
+    <meta property='og:image' content="<?php echo $url; ?>img/poster-<?php echo $dateslug; ?>.jpg"/>
+    <meta name="twitter:card" content="summary"/>
+    <meta name="twitter:site" content="@<?php echo $twitter; ?>"/>
+    <meta name="twitter:domain" content="<?php echo $parent_name; ?>"/>
+    <meta name="twitter:creator" content="@<?php echo $url_twitter; ?>"/>
+    <meta name="twitter:image:src"
+          content="<?php echo $url; ?>img/poster-<?php echo $dateslug; ?>.jpg"/>
+
+
+    <meta name="viewport" content="width=device-width">
+
+    <link href='http://fonts.googleapis.com/css?family=Jacques+Francois' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style>
+        body {
+            padding-top: 60px;
+            padding-bottom: 40px;
+        }
+    </style>
+    <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+    <link rel="stylesheet" href="css/webicons.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 7]>
+<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to
+    improve your experience.</p>
+<![endif]-->
+
+<!-- This code is taken from http://twitter.github.com/bootstrap/examples/hero.html -->
+
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="brand" href="http://www.eventbrite.com/event/7706515395">Click here to buy tickets online!</a>
+        </div>
+        <!--/.nav-collapse -->
+    </div>
+</div>
+</div>
+
+<div class="container">
+
+    <!-- Example row of columns -->
+    <div class="row">
+        <div class="span8">
+            <a href="http://www.eventbrite.com/event/7706515395"><img src='img/poster-<?php echo $dateslug; ?>.jpg'
+                                                                      alt="<?php echo $postercopy; ?>"/></a>
+        </div>
+        <div class="span4">
+            <h1>Dean Street Comedy</h1>
+
+            <p class="event-info">Wednesday, August 14<br/>9:00PM (Doors 8:45)</p>
+
+            <p class="event-info">755 Dean Street<br/>Brooklyn, NY.</p>
+
+            <p>We are here for the community presenting top New York City comedy talent, at Brooklyn prices! A couple
+                can come and watch a comedy show for 2 hours, drink and eat well for $50. Join us for the after party at
+                the bar!</p>
+
+            <p class='social'>
+                <a class="webicon youtube large" href="<?php echo $url_youtube; ?>" target="_blank">YouTube</a>
+            </p>
+
+            <h2>the Venue</h2>
+
+            <p><a href="http://www.deanstreetbrooklyn.com/" target="_blank"><img src='img/dean-street-restaurant-28.jpg'
+                                                                                 alt='Dean Street Restaurant, Brooklyn'/></a>
+            </p>
+
+            <p><a href="http://www.deanstreetbrooklyn.com/" target="_blank">Dean Street Restaurant</a><br/><span
+                    class="brooklyn">Brooklyn, NY</span></p>
+
+            <p class='social'>
+                <a class="webicon facebook large" href="<?php echo $url_facebook; ?>" target="_blank">Facebook</a>
+                <a class="webicon twitter large" href="<?php echo $url_twitter; ?>" target="_blank">Twitter</a>
+                <a class="webicon foursquare large" href="<?php echo $url_foursquare; ?>" target="_blank">Foursquare</a>
+            </p>
+
+            <p>Friends since middle school, Rob Gelardi and John Longo co-own this neighborhood bar/restaurant and
+                strive always to create a place that feels like home every time you walk in the door.</p>
+
+            <p>Two distinct areas -a front bar area and a back dining area- allow customers to decide which atmosphere
+                they prefer to enjoy our outstanding NOLA-inspired <a href="http://deanstreetbrooklyn.com/food/"
+                                                                      target="_blank">menu</a> prepared by chef Simon
+                Glenn.</p>
+
+            <h2><a href="http://www.deanstreetbrooklyn.com/" target="_blank">the Line-up</a></h2>
+
+            <h3>Host</h3>
+
+            <p>Alissa Bissonnette
+
+            <p>
+
+            <h3>Headlining</h3>
+
+            <p>Jermaine Fowler</p>
+
+            <h3>Featuring</h3>
+
+<p>Dylan Shelton</p>
+<p>Ben Kissel</p>
+<p>Henry Zebrowski</p>
+<p>Skinny B**** Jesus Meeting</p>
+<p>Chris Griggs</p>
+<p>Amber Nelson</p>			
+        </div>
+    </div>
+
+    <hr>
+
+    <footer>
+        <p>&copy; Dean Street <?php echo date('Y'); ?></p>
+    </footer>
+
+</div>
+<!-- /container -->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+
+<script src="js/vendor/bootstrap.min.js"></script>
+
+<script src="js/main.js"></script>
+
+<script>
+    var _gaq = [
+        ['_setAccount', '<?php echo $ga_account; ?>'],
+        ['_trackPageview']
+    ];
+    (function (d, t) {
+        var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        g.src = '//www.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g, s)
+    }(document, 'script'));
+</script>
+</body>
+</html>
